36844cd83d416ed38fedef56a1d406e08f1ffc8e
blueT52hz <lionel32204@gmail.com>
2025-06-19 16:45:04 +0700

    config browser for pupperteer

--- src/services/crawler.service.ts
+++ src/services/crawler.service.ts
@@ -12,8 +12,18 @@ export class CrawlerService {
 
   private async initBrowser() {
     this.browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+      ],
+      headless: true
     })
   }
 
